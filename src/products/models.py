2640de5942d3a8e9f8c9c6e0746fee4f369f2e4c
IgorSS <igorskor0912@gmail.com>
2023-08-06 21:43:05 +0300

    create products models: 'Category' and 'Product'

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'category'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='products_images')
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'product'
+
