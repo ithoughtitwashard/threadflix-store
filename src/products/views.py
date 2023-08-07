92e7dcecae84cadf64ad6db7dfcfc4a52394d9b1
IgorSS <igorskor0912@gmail.com>
2023-08-07 21:00:58 +0300

    create media directory for images

--- src/products/views.py
+++ src/products/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
-
+from .models import Category, Product
 def index(request):
-    return render(request, 'products/index.html')
+    content = {
+        'title': 'ThreadFlix',
+    }
+    return render(request, 'products/index.html', content)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    products_queryset = Product.objects.all()
+    categories_queryset = Category.objects.all().order_by('id')
+    content = {
+        'title': 'ThreadFlix catalogue',
+        'products': products_queryset,
+        'categories': categories_queryset
+    }
+    return render(request, 'products/products.html', content)
