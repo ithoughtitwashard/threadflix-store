a95fdc739a30441bd9241c4e88e997cb5ed741ad
IgorSS <igorskor0912@gmail.com>
2023-08-11 17:27:02 +0300

    improved work with admin-panel

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from products.models import Category, Product
+from products.models import Category, Product, Cart
 
 admin.site.register(Category)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', 'price', 'quantity', 'category', 'image')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class CartAdmin(admin.TabularInline):
+    model = Cart
+    fields = ('product', 'quantity')
+    extra = 0
--- src/products/models.py
+++ src/products/models.py
@@ -10,6 +10,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'Categories'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
