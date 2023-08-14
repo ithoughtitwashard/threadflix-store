2acf903f183410712108a9813039c83f1548e7d8
IgorSS <igorskor0912@gmail.com>
2023-08-14 16:01:38 +0300

    Create EmailVerification model

--- src/clients/models.py
+++ src/clients/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
+    is_verified = models.BooleanField(default=False)
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return f'EmailVerification {self.user}'
