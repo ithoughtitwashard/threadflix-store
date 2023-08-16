b86d5b44fcb1b060b4aff5c5f0ba29faa60ad558
IgorSS <igorskor0912@gmail.com>
2023-08-17 00:34:04 +0300

    create test_paginator_products_list for ProductsListView; create default test _status_ok_and_products_template_test

--- src/products/tests/test_views.py
+++ src/products/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from http import HTTPStatus
 
-from products.models import Product
+from products.models import Product, Category
 
 
 class IndexViewTestCase(TestCase):
@@ -23,23 +23,46 @@ class ProductsListViewTestCase(TestCase):
 
         products = Product.objects.all()
         products_page_1 = products[:3]
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(response, 'products/products.html')
+
+        self._status_ok_and_products_template_test(response)
         self.assertEqual(list(response.context_data['object_list']), list(products_page_1))
 
     def test_category_products_list(self):
-        url_cloth = reverse('products:category', kwargs={'category_id': 1})  # Cloth
-        url_accessories = reverse('products:category', kwargs={'category_id': 3})  # Accessories
+        category_cloth = Category.objects.get(id=1)
+        category_accessories = Category.objects.get(id=3)
+
+        url_cloth = reverse('products:category', kwargs={'category_id': category_cloth.id})
+        url_accessories = reverse('products:category', kwargs={'category_id': category_accessories.id})
+
         response_cloth = self.client.get(url_cloth)
         response_accessories = self.client.get(url_accessories)
 
-        self.assertEqual(response_cloth.status_code, HTTPStatus.OK)
-        self.assertEqual(response_accessories.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(response_cloth, 'products/products.html')
-        self.assertTemplateUsed(response_accessories, 'products/products.html')
+        self._status_ok_and_products_template_test(response_cloth)
+        self._status_ok_and_products_template_test(response_accessories)
 
         products_cloth = Product.objects.filter(category_id=1)
         products_accessories = Product.objects.filter(category_id=3)
 
         self.assertEqual(list(response_cloth.context_data['object_list']), list(products_cloth))
         self.assertEqual(list(response_accessories.context_data['object_list']), list(products_accessories))
+
+    def test_paginator_products_list(self):
+        url_page_1 = reverse('products:paginator', kwargs={'page': 1})
+        url_page_2 = reverse('products:paginator', kwargs={'page': 2})
+
+        response_page_1 = self.client.get(url_page_1)
+        response_page_2 = self.client.get(url_page_2)
+
+        self._status_ok_and_products_template_test(response_page_1)
+        self._status_ok_and_products_template_test(response_page_2)
+
+        products = Product.objects.all()
+        products_page_1 = products[:3]
+        products_page_2 = products[3:]
+
+        self.assertEqual(list(response_page_1.context_data['object_list']), list(products_page_1))
+        self.assertEqual(list(response_page_2.context_data['object_list']), list(products_page_2))
+
+    def _status_ok_and_products_template_test(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'products/products.html')
