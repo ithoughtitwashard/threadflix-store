854ad1629570ffa2d3bad6a08e25623b9080a206
IgorSS <igorskor0912@gmail.com>
2023-08-20 22:54:54 +0300

    re-create OrderCreateView

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from default.views import TitleMixin
-from products.models import Cart
+from orders.forms import OrderCreateForm
+from orders.models import Order
 
 
-class OrderCreateView(TitleMixin, ListView):
+class OrderCreateView(TitleMixin, CreateView):
     title = 'Make an order'
-    model = Cart
+    model = Order
+    form_class = OrderCreateForm
     template_name = 'orders/order-create.html'
+    success_url = reverse_lazy('orders:create')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(OrderCreateView, self).form_valid(form)
